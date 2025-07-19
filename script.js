abbaffedaaa7e8c7eff37a9c0db62da17b6b6af2
dipangshu22 <dipangshu22@gmail.com>
2025-07-19 22:08:55 +0530

    function2

--- script.js
+++ script.js
@@ -15,10 +15,34 @@ let win_pattern=[
 
 boxes.forEach((box)=>{
     box.addEventListener('click',()=>{
-        console.log("clicked");
-        box.innerText="hello "
+        console.log();
+       if(turn){
+        box.innerText='O'
+        turn=false;}
+        else{
+            box.innerText='X'
+            turn=true
+        }
+
+        box.disabled=true
+        checkwinner()
+
     })
+    
 })
 
+const checkwinner=()=>{
+    for(let data of win_pattern){
+        // console.log(data[0],data[1],data[2])
+        // console.log(boxes[data[0]].innerText,boxes[data[1]].innerText,boxes[data[2]].innerText)
+        let pos1=boxes[data[0]].innerText
+        let pos2=boxes[data[1]].innerText
+        let pos3=boxes[data[2]].innerText
 
+        if(pos1!='' && pos2!='' && pos3!=''){
+            if(pos1===pos2 && pos2==pos3)
+                console.log("winner is " ,pos1)
+        }
+    }
+}
     
